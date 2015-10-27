020810d39541b0154a61febb301a4720f79285bc
Alan Guerrero <alanguerrero@wireless-10-147-182-145.public.utexas.edu>
2015-10-27 17:17:55 -0500

    Initial Commit

---
+++ LifeShare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LifeShare
+//
+//  Created by Alan Guerrero on 10/27/15.
+//  Copyright © 2015 CS378. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
